534ea57e71d33f1b7630ef29733e7bb1e4bfad3d
Oskar Truffer <ot@studer-raimann.ch>
2016-12-05 11:44:52 +0100

    initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+TestRepositoryObject
+-------------------
+
+This is an example Plugin with minimal features for the Repository Object Plugin Slot.
+
+```
+mkdir -p Customizing/global/plugins/Services/Repository/RepositoryObject/TestRepositoryObject
+cd Customizing/global/plugins/Services/Repository/RepositoryObject/TestRepositoryObject
+git clone https://github.com/ILIAS-eLearning/TestRepositoryObject.git
+```
---
+++ plugin.php
@@ -0,0 +1,14 @@
+d = "xtst";
+ 
+// code version; must be changed for all code changes
+$version = "0.0.1";
+  
+// ilias min and max version; must always reflect the versions that should
+// run with the plugin
+$ilias_min_version = "5.1.0";
+$ilias_max_version = "5.2.999";
+   
+// optional, but useful: Add one or more responsible persons and a contact email
+$responsible = "Alex Killing";
+$responsible_mail = "alex.killing at gmx.de";
+?>
