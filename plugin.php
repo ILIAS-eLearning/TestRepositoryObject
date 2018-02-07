695aa81a3a1b57ee003ec7d7226e0160df43620a
Fabian Schmid <fs@studer-raimann.ch>
2018-02-07 09:34:07 +0100

    supported versions

--- plugin.php
+++ plugin.php
@@ -2,12 +2,12 @@
 $id = "xtst";
  
 // code version; must be changed for all code changes
-$version = "0.0.5";
+$version = "0.0.6";
   
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.1.0";
-$ilias_max_version = "5.3.999";
+$ilias_min_version = "5.4.0";
+$ilias_max_version = "5.4.999";
    
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "ILIAS Development";
