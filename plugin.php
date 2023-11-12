e7a4fdce42a5b753b21247fcdfb58ca0c373f14d
Alexander Killing <killing@leifos.de>
2023-11-12 09:57:37 +0100

    ilias 9 support

--- plugin.php
+++ plugin.php
@@ -2,12 +2,12 @@
 $id = "xtst";
  
 // code version; must be changed for all code changes
-$version = "0.0.7";
+$version = "0.0.8";
   
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "6.0";
-$ilias_max_version = "8.999";
+$ilias_max_version = "9.999";
    
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Alexander Killing";
