6792b006abca623c5fd58e4815337ef80b58fd5c
Fabian Wolf <wolf@ilias.de>
2025-02-07 10:15:14 +0100

    Update plugin.php
    ilias 10 support

--- plugin.php
+++ plugin.php
@@ -2,12 +2,12 @@
 $id = "xtst";
  
 // code version; must be changed for all code changes
-$version = "0.0.8";
+$version = "0.0.10";
   
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "6.0";
-$ilias_max_version = "9.999";
+$ilias_max_version = "10.999";
    
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Alexander Killing";
